ff16a0202d7e8bf04f62d943189d3c09680cc0bf
Christophe Thibaut <cthibauttof@gmail.com>
2025-08-10 21:22:42 +0200

    changed with Ammeraal program

--- squares.lisp
+++ squares.lisp
@@ -1,5 +1,57 @@
 (defparameter *pi* 3.14159265359)
 (defparameter *half-pi* (/ *pi* 2))
+;
+;
+; /* SQUARES: This program draws 50 squares inside each other */
+;   main ()
+;   { float xA, yA, xB, yB, xC, yC, xD, yD,
+;      xxA, yyA, xxB, yyB, xxC, yyC,xxD, yyD, p ,q ;
+;      int i;
+;      p=0.95; q=1.0-p;
+;      xA=2.0; xB=8.0; xC=8.0; xD=2.0;
+;      yA=0.5; yB=0.5; yC=6.5; yD=6.5;
+;      intgr();
+;      for (i=0; i<50; i++)
+;      { move(xA, yA);
+;         draw(xB, yB); draw(xC, yC); draw(xD, yD); draw(xA, yA);
+;         xxA=p*xA+q*xB; yyA=p*yA+q*yB; xxB=p*xB+q*xC; yyB=p*yB+q*yC;
+;         xxC=p*xC+q*xD; yyC=p*yC+q*yD; xxD=p*xD+q*xA; yyD=p*yD+q*yA;
+;         xA=xxA; xB=xxB; xC=xxC; xD=xxD;
+;         yA=yyA; yB=yyB; yC=yyC; yD=yyD;
+;      }
+;      endgr();
+;   }
+
+
+(defun x (coord)
+  (car coord))
+
+(defun y (coord)
+  (cdr coord))
+
+(defun coord (x y)
+  (cons x y))
+
+(defun squares (a b c d p limit)
+  (let ((q (- 1 p)))
+
+    (defun square-aux (a b c d counter)
+      (if (>= counter limit)
+        nil
+        (let ((next-a (coord (+ (* p (x a)) (* q (x b))) (+ (* p (y a)) (* q (y b)))))
+              (next-b (coord (+ (* p (x b)) (* q (x c))) (+ (* p (y b)) (* q (y c)))))
+              (next-c (coord (+ (* p (x c)) (* q (x d))) (+ (* p (y c)) (* q (y d)))))
+              (next-d (coord (+ (* p (x d)) (* q (x a))) (+ (* p (y d)) (* q (y a))))))
+          (append (list
+                    'moveto (round-coord a)
+                    'lineto (round-coord b)
+                    'lineto (round-coord c)
+                    'lineto (round-coord d)
+                    'lineto (round-coord a))
+                  (square-aux next-a next-b next-c next-d (1+ counter))))))
+    (square-aux a b c d 0)))
+
+
 
 (defun round-coord (coord)
   (cons (round (car coord)) (round (cdr coord))))
@@ -58,6 +110,11 @@
     (format t "</body>~%")
     (format t "</html>~%")))
 
-(render 1000 1000 (square (cons 0 0) 1000.0 0.0 0.010))
+(render 1400 700 
+        (append (squares (coord 0 0) (coord 350 0) (coord 350 350) (coord 0 350) 0.85 100)
+                (squares (coord 350 0) (coord 700 0) (coord 700 350) (coord 350 350) 0.15 100)
+                (squares (coord 0 350) (coord 350 350) (coord 350 700) (coord 0 700) 0.15 100)
+                (squares (coord 350 350) (coord 700 350) (coord 700 700) (coord 350 700) 0.85 100)
+                ))
 (sb-ext:quit)
 
